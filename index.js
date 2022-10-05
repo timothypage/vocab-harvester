1934feeced3b8d9c62992c9854ea44841b52a0f6
Tim Zwolak <timothypage@gmail.com>
2022-10-05 09:18:07 -0600

    formatting and format configuration

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const fs = require('node:fs');
+const fs = require('node:fs')
 
-const axios = require('axios').default;
-const uuid = require('uuid');
+const axios = require('axios').default
+const uuid = require('uuid')
 
-const baseUrl = 'https://www.sciencebase.gov/vocab/categories';
-const baseId = '4f4e475ee4b07f02db47df09';
-const UUID_V5_NAMESPACE = uuid.v5(baseUrl, uuid.v5.URL);
+const baseUrl = 'https://www.sciencebase.gov/vocab/categories'
+const baseId = '4f4e475ee4b07f02db47df09'
+const UUID_V5_NAMESPACE = uuid.v5(baseUrl, uuid.v5.URL)
 
-const sleep = ms => new Promise(r => setTimeout(r, ms));
+const sleep = ms => new Promise(r => setTimeout(r, ms))
 
 const getNode = async (parentId, nodeType) => {
   let params = {
@@ -16,51 +16,51 @@ const getNode = async (parentId, nodeType) => {
     max: 10,
     offset: 0,
     format: 'json'
-  };
+  }
 
   // TODO handle pagination
   const response = await axios
     .get(`${baseUrl}/get`, { params })
-    .then(response => response.data);
+    .then(response => response.data)
 
-  const fetched = response.list.length;
-  const total = response.total;
-  let list = response.list;
+  const fetched = response.list.length
+  const total = response.total
+  let list = response.list
 
-  console.log('total', total);
-  console.log('list.length', list.length);
+  console.log('total', total)
+  console.log('list.length', list.length)
 
   while (list.length < total) {
-    await sleep(1000);
+    await sleep(1000)
 
-    params.offset += 10;
+    params.offset += 10
 
-    console.log('fetching next page');
+    console.log('fetching next page')
     const nextResponse = await axios
       .get(`${baseUrl}/get`, { params })
-      .then(response => response.data);
+      .then(response => response.data)
 
-    list = list.concat(nextResponse.list);
-    console.log('total', total);
-    console.log('list.length', list.length);
+    list = list.concat(nextResponse.list)
+    console.log('total', total)
+    console.log('list.length', list.length)
   }
 
-  return { list };
-};
+  return { list }
+}
 
 const populateVocabulary = async (list, vocabulary, parentUuid) => {
   for (const item of list) {
-    await sleep(1000);
-    console.log();
-    console.log('populating:');
-    console.log('id:', item.id);
-    console.log('name:', item.name);
-    console.log('nodeType:', item.nodeType);
+    await sleep(1000)
+    console.log()
+    console.log('populating:')
+    console.log('id:', item.id)
+    console.log('name:', item.name)
+    console.log('nodeType:', item.nodeType)
 
-    const itemUuid = uuid.v5(item.id, UUID_V5_NAMESPACE);
+    const itemUuid = uuid.v5(item.id, UUID_V5_NAMESPACE)
 
     if (item.nodeType === 'vocabulary') {
-      let terms = [];
+      let terms = []
 
       vocabulary.push({
         uuid: itemUuid,
@@ -68,10 +68,10 @@ const populateVocabulary = async (list, vocabulary, parentUuid) => {
         label: item.name,
         definition: item.description || '',
         children: terms
-      });
+      })
 
-      const termNode = await getNode(item.id, 'term');
-      console.log('termNode length:', termNode.list.length);
+      const termNode = await getNode(item.id, 'term')
+      console.log('termNode length:', termNode.list.length)
 
       for (const termItem of termNode.list) {
         terms.push({
@@ -79,13 +79,13 @@ const populateVocabulary = async (list, vocabulary, parentUuid) => {
           broader: itemUuid,
           label: termItem.name,
           definition: termItem.description
-        });
+        })
       }
     } else {
-      const node = await getNode(item.id);
-      console.log('node.list.length', node.list.length);
+      const node = await getNode(item.id)
+      console.log('node.list.length', node.list.length)
 
-      let children = [];
+      let children = []
 
       vocabulary.push({
         uuid: itemUuid,
@@ -93,26 +93,26 @@ const populateVocabulary = async (list, vocabulary, parentUuid) => {
         label: item.name,
         definition: item.description || '',
         children
-      });
+      })
 
-      await populateVocabulary(node.list, children, itemUuid);
+      await populateVocabulary(node.list, children, itemUuid)
     }
   }
-};
+}
 
 async function main () {
-  const rootNode = await getNode(baseId);
+  const rootNode = await getNode(baseId)
 
-  let vocabulary = [];
-  const rootUuid = uuid.v5(baseId, UUID_V5_NAMESPACE);
-  await populateVocabulary(rootNode.list, vocabulary, rootUuid);
+  let vocabulary = []
+  const rootUuid = uuid.v5(baseId, UUID_V5_NAMESPACE)
+  await populateVocabulary(rootNode.list, vocabulary, rootUuid)
 
-  fs.writeFileSync('./vocabulary.json', JSON.stringify(vocabulary, null, 2));
+  fs.writeFileSync('./vocabulary.json', JSON.stringify(vocabulary, null, 2))
 }
 
 main()
   .then(() => process.exit())
   .catch(e => {
-    console.log(e);
-    process.exit(1);
-  });
+    console.log(e)
+    process.exit(1)
+  })
